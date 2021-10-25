ff594b3600257e5ff3f9fe0bcb231e4f9f2169c7
Aleksi Cohen <alcohen@student.hive.fi>
2021-10-25 19:11:19 +0300

    very useful comment

---
+++ group_2.h
@@ -0,0 +1,9 @@
+/* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
+/* > Cucumber-flavoured header           < */
+/* > --> filename.c <---                 < */
+/* >                         :::         < */
+/* > By: group_2            +   +        < */
+/* >                      4    :         < */
+/* >                          :          < */
+/* >                        ::::.fi      < */
+/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */
